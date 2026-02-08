c6303e63db7de97f1fd5faf9753ba3aa436c9dbc
camille-saint <camille-saint@votre-conseiller-urssaf.local>
2026-02-08 14:40:58 +0100

    animate title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,25 @@
 function Header({ themePicker }) {
   return (
     <header className="header">
-      <div className="container">
-        <h1 className="logo">Coucou Camille</h1>
+      <div className="container header-container">
+        <div className="logo-wrapper">
+          <div className="logo-marquee">
+            <span className="logo logo-archivo">coucou camille</span>
+            <span className="logo logo-flor">coucou camille</span>
+            <span className="logo logo-fungal">coucou camille</span>
+            <span className="logo logo-garamont">coucou camille</span>
+            <span className="logo logo-mess">coucou camille</span>
+            <span className="logo logo-nimbus">coucou camille</span>
+            <span className="logo logo-quarantype">coucou camille</span>
+            <span className="logo logo-archivo">coucou camille</span>
+            <span className="logo logo-flor">coucou camille</span>
+            <span className="logo logo-fungal">coucou camille</span>
+            <span className="logo logo-garamont">coucou camille</span>
+            <span className="logo logo-mess">coucou camille</span>
+            <span className="logo logo-nimbus">coucou camille</span>
+            <span className="logo logo-quarantype">coucou camille</span>
+          </div>
+        </div>
         {themePicker}
       </div>
     </header>
